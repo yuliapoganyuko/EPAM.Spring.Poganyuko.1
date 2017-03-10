55cba69e36c7e88e05d8a2ea4bae46078b4373ec
Yulia Poganyuko <yulia.poganyuko@gmail.com>
2017-03-11 01:26:58 +0300

    The algorithm of maximum search was fixed. Project Task1Console was added

---
+++ EPAM.Spring.Poganyuko.1/Task1Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Task1Logic;
+
+namespace Task1Console
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] array = GetArray();
+            Console.WriteLine(MaximalElementOfArray.GetMaximalElement(array));
+            Console.ReadKey();
+        }
+
+        private static int[] GetArray()
+        {
+            Console.WriteLine("Enter the number of array elements:");
+            int count = GetInt();
+
+            int[] sortedArray = new int[count];
+
+            Console.WriteLine("Enter array elements:");
+            for (int index = 0; index < count; index++)
+            {
+                sortedArray[index] = GetInt();
+            }
+            return sortedArray;
+        }
+        
+        private static int GetInt()
+        {
+            int number;
+            while (!Int32.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("You've entered not a number");
+            }
+            return number;
+        }
+    }
+    
+}
---
+++ EPAM.Spring.Poganyuko.1/Task1Logic/MaximalElementOfArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Logic
+{
+    public static class MaximalElementOfArray
+    {
+        #region Public Method
+
+        public static int GetMaximalElement(int[] array)
+        {
+            return GetMax(array, Int32.MinValue);
+        }
+
+        #endregion
+
+        #region Private Method
+
+        private static int GetMax(int[] array, int largest)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array[0] > largest)
+                largest = array[0];
+            if (array.Length != 1)
+            {
+                int[] newArray = new int[array.Length - 1];
+                Array.Copy(array, 1, newArray, 0, array.Length - 1);
+                array = newArray;
+                largest = GetMax(array, largest);
+            }
+            return largest;
+        }
+
+        #endregion
+    }
+}
