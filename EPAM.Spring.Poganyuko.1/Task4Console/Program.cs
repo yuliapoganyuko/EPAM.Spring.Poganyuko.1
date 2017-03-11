ada9fffebcf6f2f42b9910940a8b923433bff72e
Yulia Poganyuko <yulia.poganyuko@gmail.com>
2017-03-11 13:57:56 +0300

    Project Task4Console was added

---
+++ EPAM.Spring.Poganyuko.1/Task4Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task4Logic;
+
+namespace Task4Console
+{
+    class Program
+    {
+        #region Public methods
+
+        static void Main(string[] args)
+        {
+            string firstString = GetString();
+            string secondString = GetString();
+            Console.WriteLine(
+                $"Concatenated string: {StringsConcatenator.ConcatenateStrings(firstString, secondString)}");
+            Console.ReadKey();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static string GetString()
+        {
+            Console.WriteLine("Enter the string");
+            //string inputString = Console.ReadLine();
+            return Console.ReadLine();
+        }
+
+        #endregion
+    }
+}
