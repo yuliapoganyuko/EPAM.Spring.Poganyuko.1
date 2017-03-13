8b8da4bb7e312f8a51d4db508d6ae26e395266d1
Yulia Poganyuko <yulia.poganyuko@gmail.com>
2017-03-13 04:30:41 +0300

    Functionality and xml documentation were added to Task2Logic

--- EPAM.Spring.Poganyuko.1/Task2Logic/IndexOfEqualSumsSearch.cs
+++ EPAM.Spring.Poganyuko.1/Task2Logic/IndexOfEqualSumsSearch.cs
@@ -2,11 +2,41 @@
 
 namespace Task2Logic
 {
+    /// <summary>
+    /// Class for search for the index of equal sums of elements on the left side and on the right side of the index.
+    /// </summary>
     public static class IndexOfEqualSumsSearch
     {
+        #region Public methods
+
+        /// <summary>
+        /// Method searches for the index of equal sums of elements on the left side and on the right side of the index.
+        /// </summary>
+        /// <param name="array">Input array.</param>
+        /// <returns>The index of equal sums.</returns>
         public static int FindIndex(int[] array)
         {
-            return 0;
+            if (array == null)
+                throw new ArgumentNullException();
+
+            int totalSum = 0;
+            for (int i = 0; i < array.Length; i++)
+                totalSum += array[i];
+
+            int currentRightSum = totalSum;
+            int currentLeftSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                currentRightSum -= array[i];
+                if (currentLeftSum == currentRightSum)
+                    return i;
+                currentLeftSum += array[i];
+            }
+
+            return -1;
         }
+
+        #endregion
+        
     }
 }
